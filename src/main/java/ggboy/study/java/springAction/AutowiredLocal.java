cc63b414b7274c842cbed1832312ece14e1b7c96
ggboy-shakalaka <switch2018@outlook.com>
2018-09-22 23:18:07 +0800

    del

--- src/main/java/ggboy/study/java/springAction/AutowiredLocal.java
+++
@@ -1,5 +0,0 @@
-package ggboy.study.java.springAction;
-
-public interface AutowiredLocal {
-	public void action();
-}
--- src/main/java/ggboy/study/java/springAction/action/Action.java
+++
@@ -1,5 +0,0 @@
-package ggboy.study.java.springAction.action;
-
-public interface Action {
-	public void action(String str);
-}
