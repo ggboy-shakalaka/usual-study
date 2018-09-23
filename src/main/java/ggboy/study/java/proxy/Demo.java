69cc027faac0d586bca049f0a12ee92041dfd080
ggboy-shakalaka <switch2018@outlook.com>
2018-09-23 21:59:55 +0800

    proxy aop

--- src/main/java/ggboy/study/java/proxy/Demo.java
+++ src/main/java/ggboy/study/java/proxy/Demo.java
@@ -6,7 +6,11 @@ import java.lang.reflect.Proxy;
 public class Demo {
 	public static void main(String[] args) {
 		DemoInterface demo = ProxyHandler.applyProxy(new DemoImplements());
+		DemoInterface demo2 = ProxyHandler.applyProxy(DemoInterface.class);
+		System.out.println("--------invoke demo1------------");
 		System.out.println(demo.action());
+		System.out.println("--------invoke demo2------------");
+		System.out.println(demo2.action());
 	}
 }
 
@@ -24,6 +28,7 @@ class DemoImplements implements DemoInterface {
 
 class ProxyHandler {
 	@SuppressWarnings("unchecked")
+	// aop模式
 	public static <T> T applyProxy(final T obj) {
 		return (T) Proxy.newProxyInstance(ProxyHandler.class.getClassLoader(), obj.getClass().getInterfaces(),
 				(Object proxy, Method method, Object[] args) -> {
@@ -31,6 +36,16 @@ class ProxyHandler {
 					Object result = method.invoke(obj, args);
 					System.out.println("after invoke");
 					return result;
+				});
+	}
+	
+	@SuppressWarnings("unchecked")
+	// 直接代理接口模式
+	public static <T> T applyProxy(Class<T> clazz) {
+		return (T) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[] {clazz},
+				(Object proxy, Method method, Object[] args) -> {
+					System.out.println("invoke");
+					return "success";
 				});
 	}
 }
